f3262dcae9c22b32a75408263c9c8e65f409fbb5
Elena <shtanko1991@gmail.com>
2021-03-19 18:19:19 +0300

    first screen

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ const path = require ('path')
 const {CleanWebpackPlugin} = require ('clean-webpack-plugin')
 const CopyWebpackPlugin = require ('copy-webpack-plugin')
 const HtmlWebpackPlugin = require ('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: ['./src/index.js', './src/style/style.scss'],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: `[name].bundle.${new Date().getTime()}.js`
@@ -15,6 +16,9 @@ module.exports = {
         open: true
     },
     plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css'
+        }),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin ({
             template: './src/html/index.html',
@@ -32,8 +36,56 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(css|scss)$/,
-                use: ['style-loader','css-loader',  'sass-loader']
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass')
+                        }
+                    }
+                ]
+            },{
+                test: /\.(png|gif|jpe?g|svg|bmp|webp)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images',
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff2?|ttf|otf|eot)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'fonts'
+                        }
+                    }
+                ]
             }
            
         ]
