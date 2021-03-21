be15a88c5772e431c75123eaa1e2b60501077181
Elena <shtanko1991@gmail.com>
2021-03-21 22:56:11 +0300

    parallaks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 
 // import './style/style.scss'
 import 'bootstrap'
+// import Parallax from 'parallax-js'
+// const Parallax = require('parallax-js')
+
+
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
+
+var scene2 = document.getElementById('scene2');
+var parallaxInstance = new Parallax(scene2);
+
+var scene3 = document.getElementById('scene3');
+var parallaxInstance = new Parallax(scene3);
+
+var scene4 = document.getElementById('scene4');
+var parallaxInstance = new Parallax(scene4);
+
+var scene5 = document.getElementById('scene5');
+var parallaxInstance = new Parallax(scene5);
+
+var scene6 = document.getElementById('scene6');
+var parallaxInstance = new Parallax(scene6);
+
--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,7 @@ module.exports = {
     ],
     module: {
         rules: [
+           
             {
                 test: /\.js$/,
                 exclude: /(node_modules)/,
